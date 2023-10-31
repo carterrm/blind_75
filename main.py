322754e3236dee2a845b8ece57ca7c870e589093
carterrm <carter.r.madsen@gmail.com>
2023-10-31 16:22:37 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+import array
+from array import *
+from dynamic_programming import *
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #TODO -- ARRAY PROBLEMS
+    #two_sum_result = two_sum([3,2,4], 6)
+    #buy_sell_stock_result = buy_sell_stock([7,1,5,3,6,4])
+    #contains_duplicate_result = contains_duplicate([1,2,3,4,1])
+    #three_sum_result = three_sum([-2,0,1,1,2])
+
+    one = 1
+    #TODO -- DYNAMIC PROGRAMMING PROBLEMS
+    climbing_stairs_result = climbing_stairs(5)
+    three = 3
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
