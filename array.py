cec0e68bfd565a1b8b961f709c214a36a7445998
carterrm <carter.r.madsen@gmail.com>
2023-11-01 11:33:08 -0600

    Added Chat GPT's solution to Three Sum

--- array.py
+++ array.py
@@ -73,7 +73,39 @@ def three_sum(nums):
         results.append(list(tuple))
     return results
 
-        #returns a list of lists of integeres
+    # Chat GPT's solution:
+    # if len(nums) < 3:
+    #         return []
+    #
+    #     nums.sort()
+    #     results = []
+    #
+    #     for i in range(len(nums) - 2):
+    #         if i > 0 and nums[i] == nums[i - 1]:
+    #             continue
+    #
+    #         j = i + 1
+    #         k = len(nums) - 1
+    #
+    #         while j < k:
+    #             total = nums[i] + nums[j] + nums[k]
+    #             if total < 0:
+    #                 j += 1
+    #             elif total > 0:
+    #                 k -= 1
+    #             else:
+    #                 results.append([nums[i], nums[j], nums[k]])
+    #
+    #                 # To avoid duplicates
+    #                 while j < k and nums[j] == nums[j + 1]:
+    #                     j += 1
+    #                 while j < k and nums[k] == nums[k - 1]:
+    #                     k -= 1
+    #
+    #                 j += 1
+    #                 k -= 1
+    #
+    #     return results
 
 def two_sum_II(input_array, target):
     front_pointer = 0
