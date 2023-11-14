0f66bc8c3aa9798dcd483cf139a45dd7d27b7f6b
carterrm <carter.r.madsen@gmail.com>
2023-11-14 12:20:01 -0700

    Added Valid Parentheses & Palindrome solutions

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from graph import *
 from binary import *
 from tree import *
 from string_problems import *
+from matrix import *
 
 # Just to be clear, TODOs are only labeled as such so the font shows up in a different color. Nothing is actually still left to do.
 if __name__ == '__main__':
@@ -28,27 +29,6 @@ if __name__ == '__main__':
     #climbing_stairs_result = climbing_stairs(5)
     #coin_change_result = coin_change([186,419,83,408], 6249)
 
-    # TODO -- LINKED LIST PROBLEMS
-
-    # node_5 = ListNode(5)
-    # node_4 = ListNode(4,node_5)
-    # node_3 = ListNode(3,node_4)
-    # node_2 = ListNode(2)
-    # node_1 = ListNode(1, node_2)
-    #reverse_linked_list_result = reverseList(node_1)
-
-    # node_3 = ListNode(4, None)
-    # node_2 = ListNode(2, node_3)
-    # node_1 = ListNode(0, node_2)
-    #
-    # alt_node_3 = ListNode(5, None)
-    # alt_node_2 = ListNode(3, alt_node_3)
-    # alt_node_1 = ListNode(1, alt_node_2)
-
-    #detect_cycle_result = detect_cycle(node_1)
-    #mergeTwoLists_result = mergeTwoLists(node_1, alt_node_1)
-    #removeNthFromEndResult = removeNthFromEnd(node_1, 2)
-
     #TODO -- GRAPH PROBLEMS
 
     # adj_list = []
@@ -64,8 +44,34 @@ if __name__ == '__main__':
 
     #clone_graph_result = clone_graph(graph_node_1)
 
+    # TODO -- LINKED LIST PROBLEMS
+
+    # node_5 = ListNode(5)
+    # node_4 = ListNode(4,node_5)
+    # node_3 = ListNode(3,node_4)
+    # node_2 = ListNode(2)
+    # node_1 = ListNode(1, node_2)
+    # reverse_linked_list_result = reverseList(node_1)
+
+    # node_3 = ListNode(4, None)
+    # node_2 = ListNode(2, node_3)
+    # node_1 = ListNode(0, node_2)
+    #
+    # alt_node_3 = ListNode(5, None)
+    # alt_node_2 = ListNode(3, alt_node_3)
+    # alt_node_1 = ListNode(1, alt_node_2)
+
+    # detect_cycle_result = detect_cycle(node_1)
+    # mergeTwoLists_result = mergeTwoLists(node_1, alt_node_1)
+    # removeNthFromEndResult = removeNthFromEnd(node_1, 2)
+
+    #TODO -- MATRIX PROBLEMS
+    rotate_image_result = rotate_image([[1,2,3],[4,5,6],[7,8,9]])
+
     #TODO -- STRING PROBLEMS
-    valid_anagram_result = valid_anagram("anagram", "nagamarf")
+    #valid_anagram_result = valid_anagram("anagram", "nagamarf")
+    #valid_parentheses_result = valid_parentheses("(")
+    #valid_palindrome_result = valid_palindrome("A man, a plan, a canal: Panama")
 
     #TODO -- TREE PROBLEMS
     # tree_node_5 = TreeNode(7, None, None)
--- string_problems.py
+++ string_problems.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def valid_anagram(s,t) -> bool:
     #Memory performance is excellent (between 63.60% and 98.67%), runtime performance varies widely (12.94% to 62.97%)
     table = dict()
@@ -16,5 +18,40 @@ def valid_anagram(s,t) -> bool:
             table.pop(t[i])
     if len(table) > 0:
         return False
+    return True
+
+def valid_parentheses(s: str) -> bool:
+    parens = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
+    soln_stack = deque()
+    for i in range(0, len(s)):
+        if s[i] in parens:
+            soln_stack.appendleft(s[i])
+        else:
+            if len(soln_stack) > 0:
+                popped = soln_stack.popleft()
+                if s[i] != parens[popped]:
+                    return False
+            else:
+                return False
+    if len(soln_stack) == 0:
+        return True
+    else: return False
+
+def valid_palindrome(s) -> bool:
+    import re
+    s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+    half_length = len(s) // 2
+    is_odd = True if len(s) % 2 == 1 else False
+    i = half_length - 1
+    j = half_length if is_odd is False else half_length + 1
+    while i >= 0:
+        if s[i] != s[j]:
+            return False
+        i -= 1
+        j += 1
     return True
 
